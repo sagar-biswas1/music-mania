ccb0f217204e4ccb60b28a45f00622ef8f1172a3
sagar-biswas1 <bsagor99@gmail.com>
2023-10-03 18:39:09 +0600

    upload controller done

--- src/api/v1/song/controllers/upload.js
+++ src/api/v1/song/controllers/upload.js
@@ -1,34 +1,55 @@
-const { getAudioDuration, processToOgg, processToHls } = require("../../../../queues/audioProcessor");
+const {
+  getAudioDuration,
+  processToOgg,
+  processToHls,
+} = require("../../../../queues/audioProcessor");
 const songService = require("../../../../lib/song");
 const upload = async (req, res, next) => {
-  const getDuration = await getAudioDuration(`./${req.file.path}`);
+  try {
+    const getDuration = await getAudioDuration(`./${req.file.path}`);
 
-  const { title, artist, cover, status, category } = req.body;
-  const dbPayload = {
-    title,
-    artist,
-    cover,
-    status,
-    category,
-    fileName: req.file.filename,
-    originalName: req.file.originalname,
-    audioLink: req.file.path,
-    durationInSeconds: getDuration.durationInSeconds,
-    uploadedBy: "651aa1203ebfcf4ba6b30bb1",
-  };
+    const { title, artist, cover, status, category } = req.body;
+    const dbPayload = {
+      title,
+      artist,
+      cover,
+      status,
+      category,
+      fileName: req.file.filename,
+      originalName: req.file.originalname,
+      audioLink: req.file.path,
+      durationInSeconds: getDuration.durationInSeconds,
+      uploadedBy: "651aa1203ebfcf4ba6b30bb1",
+    };
 
-  const result = await songService.upload(dbPayload);
-  const response = {
-    message: "Song uploaded successfully.",
-    song_data: {
-      ...result,
-    },
-    links: {
-      self: `/songs/${result._id}`,
-    },
-  };
-  res.status(201).json(response);
-  processToHls(req.file.path,`./uploads/hls_output`)
+    const result = await songService.upload(dbPayload);
+    //todo : handle processToHls in the background after sending response
+    //while processed notify user using websocket connection
+    const { processedPath } = await processToHls(
+      req.file.path,
+      `./uploads/hls_output`
+    );
+
+    if (!processedPath) {
+      return res
+        .status(500)
+        .json({ message: "Couldn't process uploaded file" });
+    }
+    const response = {
+      message: "Song uploaded successfully ...",
+      song_data: {
+        ...result,
+        processedPath,
+      },
+      links: {
+        self: `/songs/${result._id}`,
+      },
+    };
+
+    res.status(201).json(response);
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = upload;
--- src/queues/audioProcessor.js
+++ src/queues/audioProcessor.js
@@ -1,7 +1,7 @@
 const ffmpeg = require("fluent-ffmpeg");
 
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
 const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
@@ -21,52 +21,56 @@ function getAudioDuration(audioFilePath) {
   });
 }
 
-
-
-const processToOgg=(inputFilePath,outputFolder)=>{
+const processToOgg = (inputFilePath, outputFolder) => {
   const fileExt = path.extname(inputFilePath);
   const fileNameWithoutExt = path.basename(inputFilePath, fileExt);
 
   const outputFilePath = `${outputFolder}/${fileNameWithoutExt}.ogg`;
-  console.log(fileExt, fileNameWithoutExt,outputFilePath)
-   ffmpeg()
-  .input(inputFilePath)
-  .audioCodec('libvorbis') // Set the audio codec to libvorbis for Ogg Vorbis format
-  .output(outputFilePath)
-  .on('end', () => {
-    console.log('Conversion finished.');
-  })
-  .on('error', (err) => {
-    console.error('Error:', err);
-  })
-  .run();
-}
+  console.log(fileExt, fileNameWithoutExt, outputFilePath);
+  ffmpeg()
+    .input(inputFilePath)
+    .audioCodec("libvorbis") // Set the audio codec to libvorbis for Ogg Vorbis format
+    .output(outputFilePath)
+    .on("end", () => {
+      console.log("Conversion finished.");
+    })
+    .on("error", (err) => {
+      console.error("Error:", err);
+    })
+    .run();
+};
 
-const bitrates = [128, 64, 32]; 
-const processToHls=(inputFilePath,outputFolder)=>{
-const fileExt = path.extname(inputFilePath);
-const fileNameWithoutExt = path.basename(inputFilePath, fileExt);
+const bitrates = [128, 64, 32];
+const processToHls = (inputFilePath, outputFolder) => {
+  const fileExt = path.extname(inputFilePath);
+  const fileNameWithoutExt = path.basename(inputFilePath, fileExt);
 
-const outputFilePath = `${outputFolder}/${fileNameWithoutExt}.m3u8`;
-// const outputFilePath = path.join(outputFolder, `segment_${index}.m4s`)
-console.log({inputFilePath})
-ffmpeg()
-  .input(inputFilePath)
-  .inputFormat('mp3')
-  .videoCodec('copy') // No video codec
-  .audioCodec('aac')  // AAC audio codec for HLS
-  .addOption('-hls_time', '3') // Set segment duration to 3 seconds (adjust as needed)
-  .addOption('-hls_list_size', '0') // Generate an indefinite playlist
-  .addOption('-hls_segment_filename', `${outputFolder}/${fileNameWithoutExt}_%02d.ts`) // Set segment filenames
-  .addOption('-hls_allow_cache', '1') // Allow caching on the client-side
-  .output(outputFilePath)
-  .on('end', () => {
-    console.log('Conversion finished.');
-  })
-  .on('error', (err) => {
-    console.error('Error:', err);
-  })
-  .run();
+  const outputFilePath = `${outputFolder}/${fileNameWithoutExt}.m3u8`;
+  // const outputFilePath = path.join(outputFolder, `segment_${index}.m4s`)
+  // console.log({inputFilePath})
 
-}
-module.exports = { getAudioDuration,processToOgg ,processToHls};
+  return new Promise(function (resolve, reject) {
+    ffmpeg()
+      .input(inputFilePath)
+      .inputFormat("mp3")
+      .videoCodec("copy") // No video codec
+      .audioCodec("aac") // AAC audio codec for HLS
+      .addOption("-hls_time", "3") // Set segment duration to 3 seconds (adjust as needed)
+      .addOption("-hls_list_size", "0") // Generate an indefinite playlist
+      .addOption(
+        "-hls_segment_filename",
+        `${outputFolder}/${fileNameWithoutExt}_%02d.ts`
+      ) // Set segment filenames
+      .addOption("-hls_allow_cache", "1") // Allow caching on the client-side
+      .output(outputFilePath)
+      .on("end", () => {
+        console.log("Conversion finished.");
+        resolve({ processedPath: outputFilePath });
+      })
+      .on("error", (err) => {
+        console.error("Error:", err);
+      })
+      .run();
+  });
+};
+module.exports = { getAudioDuration, processToOgg, processToHls };
